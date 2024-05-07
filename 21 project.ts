d49e067208b79cf07732efc5bad538f8a7b31564
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 21:47:42 -0700

    Add files via upload

---
+++ 21 project.ts
@@ -0,0 +1,27 @@
+interface person{
+    age:number,
+    name:string,
+    nationality:string,
+    student:boolean,
+}
+let person={
+    age:21,
+    name :'yasir',
+    nationality :'pakistan',
+    student :true
+}
+//print person
+console.log(person);
+
+interface car{
+    maker: string,
+    color:string,
+    automatic:boolean,
+}
+let car={
+    maker:'civic',
+    color:'white',
+    automatic:'true',
+}
+//print car
+console.log(car);
