c526857b65949d939472679bf9600b9a0403c43e
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 21:39:04 -0700

    Add files via upload

---
+++ 14 guest list.ts
@@ -0,0 +1,5 @@
+let guest_list : string []=['ali','akbar','arham','amir'];
+for (let i=0; i<guest_list.length; i++){
+    console.log ('respected sir/' + guest_list[i] + ', \nwe invited you dinner tomorrow.\n\nThank you')
+} 
+export {guest_list}
