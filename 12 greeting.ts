5c03ae4e3e9855c76cba7151f86b1eac13077cc4
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 21:36:40 -0700

    Add files via upload

---
+++ 12 greeting.ts
@@ -0,0 +1,5 @@
+let members:string[]=['kanwal','Iqra','sadia','naila','ayesha'];
+let message:string='kal birthday kis ki hai';
+for(let i =0; i < members.length; i++){
+    console.log(members[i]);
+}
