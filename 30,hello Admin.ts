bf6ed5314beed3c6e53599df6899060674439de0
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 21:58:29 -0700

    Add files via upload

---
+++ 30,hello Admin.ts
@@ -0,0 +1,9 @@
+const userName:string[]=['admin','bugsbunny','daffyduck','sylvester','tweety'];
+userName.forEach (userName=>{
+    if(userName ==='admin')
+        console.log('Hello admin, would you like to see a status report?');
+
+{
+    console.log(`Hellow ${userName}, than you for logging in again`);
+}
+})
