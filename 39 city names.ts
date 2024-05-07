d8f73fab360ca782b9218310b46ce6e3e4c0c97f
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 22:04:40 -0700

    Add files via upload

---
+++ 39 city names.ts
@@ -0,0 +1,5 @@
+function city_country(city:string,country:string){return `${city} , ${country}`;
+}
+console.log(city_country('Karachi','Pakistan'));
+console.log(city_country('Toyko','Japan'));
+console.log(city_country('Paris','france'));
