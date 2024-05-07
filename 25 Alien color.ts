150e99baa25e48c6a982192ad7c5ced02c4e79db
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 21:54:38 -0700

    Add files via upload

---
+++ 25 Alien color.ts
@@ -0,0 +1,12 @@
+//create a variable called alien_color
+let alien_color:string="green"
+// white an if statement to test whether the aliens color is sky blue.If it //print a message that the player just earned 5 point.
+if(alien_color=="green"){
+console.log("the player just earned 5 point");
+}
+//write one version of this program that passes the if test and another that fails.
+//(The version that fails will have on output.)
+alien_color="yellow";
+if(alien_color=="green"){
+    console.log("The player just earned 5 points.");
+}
