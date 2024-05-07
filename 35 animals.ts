9fe13edb248afe57a841c64bb124efc9821474fb
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 22:01:53 -0700

    Add files via upload

---
+++ 35 animals.ts
@@ -0,0 +1,5 @@
+let animals:string[]=['lion','elephant','tiger'];
+for (let Animal of animals){
+    console.log(`A ${Animal} would make a great pet.`);
+}
+console.log(`Any of the animals would a great pet!`);
