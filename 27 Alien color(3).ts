c69cceba8e64afbb2f807d0fc32cbb62f1082494
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 21:56:04 -0700

    Add files via upload

---
+++ 27 Alien color(3).ts
@@ -0,0 +1,42 @@
+let aliencolor:string="green";
+//version 1 of the program.
+if(aliencolor ==="green"){
+    console.log("version 1:player earned 5 points.");
+}
+else if(aliencolor ==="yellow"){
+    console.log("player earned 10 point");
+}
+else if(aliencolor ==="red"){
+    console.log("player earned 15 points.");
+}
+else{
+    console.log("please select right color")
+}
+//version 2 of the program.
+aliencolor="yellow";
+if (aliencolor==="green"){
+    console.log(" player earned 5 point.");
+}
+else if(aliencolor==="yellow"){
+    console.log("version 2:player earned 10 point.");
+}
+else if (aliencolor==="red"){
+    console.log("player earned 15 point.");
+}
+else{
+    console.log("please select right color")
+}
+//version 3 of the program.
+aliencolor="red";
+if (aliencolor==="green"){
+    console.log("player earned 5 point.");
+}
+else if (aliencolor==="yellow"){
+    console.log("player earned 10 point.");
+}
+else if (aliencolor==="red"){
+    console.log("version 3: player earned 15 point.");
+}
+else{
+    console.log("please select right colors")
+}
