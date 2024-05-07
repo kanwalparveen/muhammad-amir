30f7436f675373b6055d4744abb95044a7d89200
kanwalparveen <167170060+kanwalparveen@users.noreply.github.com>
2024-05-06 22:02:33 -0700

    Add files via upload

---
+++ 36 Tshirt.ts
@@ -0,0 +1,8 @@
+function make_shirt(size:string,text:string){
+    console.log(`creating A ${size} shirt with the message:${text}`);
+}
+make_shirt('large','Hello would');
+
+make_shirt('medium','Hello would');
+
+make_shirt('small','Hello world');
